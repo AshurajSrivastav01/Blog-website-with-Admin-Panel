f4f95b1555b36d93c27b419e5c3e4b4fed18de5a
LDS <ashuraj.srivastav01@gmail.com>
2025-09-03 18:28:47 +0530

    crafted some new pages for authontication like Login, register, change Password and forgot password

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Frontend Routes
 Route::view('/', 'frontend.blog', ['Title' => 'Home - Modern Blog']);
 Route::view('/about', 'frontend.about', ['Title' => 'About - Modern Blog']);
 Route::view('/contact', 'frontend.contact', ['Title' => 'Contact - Modern Blog']);
 Route::view('/Categories', 'frontend.categories', ['Title' => 'Categories - Modern Blog']);
+
+// Admin Routes
+
+
+// Authontication Routes
+Route::view('/cb-user/login', 'auth.login', ['Title' => 'User Login - Modern Blog']);
+Route::view('/cb-user/forgot-password', 'auth.forgotPassword', ['Title' => 'Forgot Password - Modern Blog']);
+Route::prefix('user')->group(function () {
+    Route::view('/register', 'auth.register', ['Title' => 'User Register - Modern Blog']);
+    Route::view('/reset-password', 'auth.changePassword', ['Title' => 'Reset Password - Modern Blog']);
+});
+
+
