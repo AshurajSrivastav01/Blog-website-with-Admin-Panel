61ef9fd97b1ffaef523ea4f58622d687a6717151
Ashuraj Srivastav <ashuraj.srivastav01@gmail.com>
2025-09-06 22:24:02 +0530

    mhtdfybyku

--- resources/views/backend/404.blade.php
+++ resources/views/backend/404.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
-    <!-- Google Fonts -->
+    <!-- Google Fonts --> 
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <style>
         :root {
